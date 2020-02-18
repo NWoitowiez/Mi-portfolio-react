59905edb547020a50d54785a4f66beef22554bcd
NWoitowiez <diego.woitowiez@alu.bue.edu.ar>
2020-02-18 12:07:24 -0300

    Agregue titulos, boton, y info, corregi el codigo

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Background from './img/cielo.jpg';
-import mainPerfil from'./img/Perfil.png';
+import mainPerfil from './img/Perfil.png';
 
 import './Header.css';
 
@@ -19,13 +19,18 @@ class Header extends Component {
                 <h1>{this.props.title}</h1>
                 <p>HTML / CSS / JS / BOOTSTRAP / REACT</p>
                 <a href="#button">{this.props.button}</a>
-                <h2>Sobre mi</h2>
+
+            <div>
+                <h1 className="Titulosinicio">Sobre mí</h1>
+                <h2 className="Textoinfo">Hola, mi nombre es Nicolas, tengo 21 años,
+                <br></br> nací en el barrio de Boedo, barrio de murga y carnaval (?. 
+                <br></br> Agrego texto que despues va a ser mejorado no os preocupeis.</h2>
+            </div>
+                <h1 className="Titulosinicio">Trabajos</h1>
+            
+                <h1 className="Titulosinicio">Hobbies</h1>
                 
-                <h2>Trabajos</h2>
-                
-                <h2>Hobbies</h2>
-                
-                <h2>Contacto</h2>
+                <h1 className="Titulosinicio">Contacto</h1>
             </header>
 
         );
