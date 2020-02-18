c9ed6cb8ef677491a6cf019e72e2a9a861c2c1d6
NWoitowiez <diego.woitowiez@alu.bue.edu.ar>
2020-02-18 05:32:46 -0300

    Agregue foto de perfil en el inicio y mas descripciones

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import Background from './img/cielo.jpg';
+import mainPerfil from'./img/Perfil.png';
 
 import './Header.css';
 
 const myStyles = {
     backgroundImage: `url( ${Background} )`,
-    height: '50vh',
+    height: '500vh',
     BackgroundSize: 'cover'
-
 }
 
 class Header extends Component {
@@ -15,9 +15,17 @@ class Header extends Component {
         return (
             
             <header style= {myStyles}>
+                <img src={mainPerfil}></img>
                 <h1>{this.props.title}</h1>
                 <p>HTML / CSS / JS / BOOTSTRAP / REACT</p>
                 <a href="#button">{this.props.button}</a>
+                <h2>Sobre mi</h2>
+                
+                <h2>Trabajos</h2>
+                
+                <h2>Hobbies</h2>
+                
+                <h2>Contacto</h2>
             </header>
 
         );
